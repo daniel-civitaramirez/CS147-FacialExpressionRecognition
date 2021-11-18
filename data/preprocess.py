20f72044fe0d4ebc0aeb02814c422a817fb11f05
Daniel Civita Ramirez <dani.civitaramirez@gmail.com>
2021-11-18 16:40:10 -0500

    preprocess

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,10 +1,17 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+import gzip
 
 _DEFAULT_TRAIN_FILEPATH = ['train_data_1.gz', 'train_data_2.gz', 'train_data_3.gz', 'train_data_4.gz']
 _DEFAULT_TEST_FILEPATH = 'test_data.gz'
 
 
 def get_data(train_data_file_paths: list = _DEFAULT_TRAIN_FILEPATH, test_data_file_path: str = _DEFAULT_TEST_FILEPATH):
-    pass
+    test_data = pd.read_csv(test_data_file_path, compression='gzip')
+
+
+
+
+if __name__ == '__main__':
+    get_data()
