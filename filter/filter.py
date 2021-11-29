0e66ce6b111f224b89a156a5872e366a96829b81
Jimena Barrio Arbona <jimenabarrioarbona@jimenas-mbp.myfiosgateway.com>
2021-11-29 12:18:32 -0500

    filter.py

--- filter/filter.py
+++ filter/filter.py
@@ -13,6 +13,5 @@ def filter_application(image_name, emotion):
     change_bg.load_pascalvoc_model("deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
     color_dict = {
         0 : (128,0,0), 1: (0,128,0), 2: (0,0,0), 3: (255,255,0), 4: (0,0,128), 5: (255,255,255), 6: (128,128,128)
-
     }
     change_bg.color_bg(image_name, colors = color_dict[emotion], output_image_name='output_im.png')
