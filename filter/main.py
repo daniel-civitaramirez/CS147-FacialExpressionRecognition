f4286cbd32729b30d2af06ec6fcb3e1dadec4baf
Jimena Barrio Arbona <jimenabarrioarbona@jimenas-mbp.myfiosgateway.com>
2021-12-05 17:36:56 -0500

    main updated waiting for the model

--- filter/main.py
+++ filter/main.py
@@ -1,5 +1,6 @@
 import os.path
 from os import path
+from numpy.lib.npyio import load
 from tensorflow.python.keras import models
 import os
 import cv2
@@ -7,9 +8,10 @@ import numpy as np
 from pixellib.tune_bg import alter_bg
 from PIL import Image
 from filter import filter_application
+from model.cnn import predictEmotion, loadModel
 
 
-def live_detection():
+def live_detection(model):
     cv2_base_dir = os.path.dirname(os.path.abspath(cv2.__file__))
     haar_model = os.path.join(cv2_base_dir, 'data/haarcascade_frontalface_default.xml')
     cascade = cv2.CascadeClassifier(haar_model)
@@ -42,20 +44,24 @@ def live_detection():
     (x,y,width,height) = cascade.detectMultiScale(gray_frame, 1.3, 7, minSize=(35, 35))[0]
 
     cropped_face = gray_frame[int(y):int(y+height),x:int(x+width)]
-    scaled_face = cv2.resize(cropped_face, (48,48), 0,0, interpolation=cv2.INTER_AREA) / 255
+    scaled_face_undivided = cv2.resize(cropped_face, (48,48), 0,0, interpolation=cv2.INTER_AREA)
+
 
     ##associate color to emotion -- pass the correct color to the output line so that it can be 
-    ##changed and shown. 
+    ##changed and shown.
+    emotion_index,_,_ = predictEmotion(model, scaled_face_undivided)
     print("emotion selected!")
 
-    filter_application(img_name, 6)
+    filter_application(img_name, emotion_index)
     print("filter applied!")
     image = Image.open('output_im.png') 
     image.show()
     
 
 def main():
-    live_detection()
+    file_name = "cnn_weights.h5"
+    model = loadModel(file_name)
+    live_detection(model)
 
 if __name__ == '__main__':
     main() 
