3db3c4ecdc530a4576a4890fae2603625a763dcd
Daniel Civita Ramirez <dani.civitaramirez@gmail.com>
2021-12-05 20:19:06 -0500

    Update cnn.py

--- model/cnn.py
+++ model/cnn.py
@@ -4,63 +4,58 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
-from regularization import RescaleLayer, DataAugmentLayer
+from regularization import DataAgumentationGenerator
 
 IMG_DIM = 48
 EMOTION_CLASSIFICATION = {0: 'Angry', 1: 'Digust', 2: 'Fear',
                           3: 'Happy', 4: 'Sad', 5: 'Surprise', 6: 'Neutral'}
 
-def ConvultionLayer():
+
+def generateModel(num_emotion=7):
     return Sequential(
         [
-            layers.Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding='SAME', activation='relu'),
+            layers.InputLayer(input_shape=(IMG_DIM, IMG_DIM, 1)),
+
+            layers.Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding='SAME', activation='relu'),
             layers.BatchNormalization(),
-            layers.MaxPool2D(pool_size=(2,2), strides=(2,2)),
+            layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
 
             layers.Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='SAME', activation='relu'),
             layers.BatchNormalization(),
             layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
 
-            layers.Conv2D(filters=512, kernel_size=(3, 3),strides=(1, 1), padding='SAME', activation='relu'),
+            layers.Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding='SAME', activation='relu'),
             layers.BatchNormalization(),
             layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
 
-            layers.Conv2D(filters=512, kernel_size=(3, 3),strides=(1, 1), padding='SAME', activation='relu'),
+            layers.Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding='SAME', activation='relu'),
             layers.BatchNormalization(),
             layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
 
             layers.Dropout(rate=0.2),
-        ]
-    )
 
-def HiddenLayer():
-    return Sequential(
-        [
-            layers.Dense(units=256, activation='relu'),
-            layers.BatchNormalization(),
-
-            layers.Dense(units=128, activation='relu'),
-            layers.BatchNormalization(),
-        
-        ]
-    )
-
-def generateModel(num_emotion=7):
-    return Sequential(
-        [
-            keras.Input(shape=(IMG_DIM, IMG_DIM, 1)),
-            RescaleLayer(),
-            DataAugmentLayer(),
-            ConvultionLayer(),
             layers.Flatten(),
-            HiddenLayer(),
+
+            layers.Dense(units=1, activation='relu'),
+            layers.BatchNormalization(),
+
+            layers.Dense(units=1, activation='relu'),
+            layers.BatchNormalization(),
+
             layers.Dense(units=num_emotion, activation='softmax')
         ]
     )
 
-def trainModel(model, x_train, y_train, x_val, y_val, epochs=50, batch_size=64):
+def trainModel(model, x_train, y_train, x_val, y_val, epochs=35, batch_size=64):
+    data_augmentation_gen = DataAgumentationGenerator()
     model.compile(optimizer='adam', loss='categorical_crossentropy',metrics=['accuracy'])
-    history = model.fit(x_train, y_train, epochs=epochs, batch_size=64, verbose=1, validation_data=(x_val, y_val))
+
+    history = model.fit_generator(
+                        data_augmentation_gen.flow(x_train, y_train, batch_size),
+                        epochs=epochs,
+                        verbose=1, 
+                        validation_data=(x_val, y_val))
+    
     return model, history
 
 def testModel(model, x_test, y_test):
@@ -75,7 +70,6 @@ def loadModel(filepath='cnn_weights.h5'):
     return keras.models.load_model(filepath)
 
 def predictEmotion(model, image):
-    image = tf.keras.utils.img_to_array(image)
     predicition = model.predict(image)[0]
     prob = np.max(predicition)
     emotion_index = np.argmax(predicition)
